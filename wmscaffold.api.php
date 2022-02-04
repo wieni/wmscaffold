447546bb7b52004b6ba1b8135183df0ba2cf9b0e
Dieter Holvoet <dieter.holvoet@gmail.com>
2022-02-04 15:28:37 +0100

    Document hooks in wmscaffold.api.php
    Fixes #22.

---
+++ wmscaffold.api.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Alter the information provided in \Drupal\wmscaffold\Annotation\ModelMethodGenerator.
+ *
+ * @param array $generators
+ *   The array of generator plugins, keyed by the machine-readable name.
+ */
+function hook_wmscaffold_model_method_generator_alter(array &$generators) {
+    $generators['link']['class'] = \Drupal\wmscaffold\Plugin\ModelMethodGenerator\FieldHelperLink::class;
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
